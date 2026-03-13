df105e96a584a91f2bfa2af1e2e5bb3f6545ae96
lemendozar <mendozaruiz.luisenrique@gmail.com>
2026-03-13 11:45:31 -0600

    Animaciones del jugador agregadas

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var sprite : Sprite2D = $PlayerSprite
+
 # velocidad máxima a la que nos moveremos
 @export var move_speed : float = 100
 # tasa de aceleración del personaje
@@ -21,6 +23,39 @@ func _physics_process(delta: float):
 	
 	# obtener la entrada
 	move_input = Input.get_axis("left", "right")
-	# movimiento
-	velocity.x = move_input * move_speed
+	# Si hay movimiento...
+	if move_input != 0:
+		# entonces la velocidad en x es interpolada entre el reposo y la aceleración
+		velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
+	# si no
+	else:
+		# velocidad en x es interpolada entre el movimiento y el "freno"
+		velocity.x = lerp(velocity.x, 0.0, braking * delta)
+	
+	# Salto
+	# Si la tecla de salto ha sido presionada y estamos en el suelo...
+	if Input.is_action_pressed("jump") and is_on_floor():
+		# entonces la velocidad en y es la fuerza de salto negativa
+		velocity.y = -jump_force
+		
 	move_and_slide()
+	
+func _process(delta: float):
+	# Si el caracter se está moviendo:
+	if velocity.x != 0:
+		# entonces si la velocidad en x es positiva, se gira el sprite
+		sprite.flip_h = velocity.x > 0
+		
+	_manage_animations()
+		
+func _manage_animations():
+	# si el caracter está en el aire:
+	if not is_on_floor():
+		# reproduce "idle"
+		$AnimationPlayer.play("jump")
+	# pero si se está moviendo:
+	elif move_input != 0:
+		$AnimationPlayer.play("move")
+	# y si nada de eso ocurre:
+	else:
+		$AnimationPlayer.play("idle")
